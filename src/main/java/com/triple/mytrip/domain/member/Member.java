216c255566534637f3d887c319877b635f362409
cares0 <cares00000@gmail.com>
2022-06-15 12:11:30 +0900

    회원 엔티티 개발
    회원 도메인 설계 예정 없음, 기본 틀만 개발

--- src/main/java/com/triple/mytrip/domain/member/Member.java
+++ src/main/java/com/triple/mytrip/domain/member/Member.java
@@ -1,12 +1,28 @@
 package com.triple.mytrip.domain.member;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id @GeneratedValue
     private Long id;
+
+    private String email;
+    private String password;
+    private String phone;
+    private String name;
+    private String nickname;
+    private String inviteCode;
+
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
