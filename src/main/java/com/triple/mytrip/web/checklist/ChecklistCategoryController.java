1a78b6e2b659210b7302231bcac1ade0cddc1057
cares0 <cares00000@gmail.com>
2022-06-16 15:18:59 +0900

    feat: 웹 계층 - 체크리스트 저장 개발

--- src/main/java/com/triple/mytrip/web/checklist/ChecklistCategoryController.java
+++ src/main/java/com/triple/mytrip/web/checklist/ChecklistCategoryController.java
@@ -1,6 +1,9 @@
 package com.triple.mytrip.web.checklist;
 
+import com.triple.mytrip.domain.checklist.Checklist;
+import com.triple.mytrip.domain.checklist.ChecklistService;
 import com.triple.mytrip.domain.checklist.category.ChecklistCategoryService;
+import com.triple.mytrip.web.checklist.form.ChecklistForm;
 import com.triple.mytrip.web.common.Result;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -9,17 +12,25 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ChecklistCategoryController {
 
-    private final ChecklistCategoryService checklistCategoryService;
+    private final ChecklistCategoryService categoryService;
+    private final ChecklistService checklistService;
+
+    @PostMapping("/checklist-categories/{categoryId}/checklist")
+    public Result<Long> saveChecklist(@PathVariable Long categoryId, @RequestBody ChecklistForm checklistForm) {
+        Checklist checklist = new Checklist(checklistForm.getName());
+        Long savedId = checklistService.save(categoryId, checklist);
+        return new Result<>(savedId);
+    }
 
     @PatchMapping("/checklist-categories/{categoryId}")
     public Result<String> editName(@PathVariable Long categoryId, @RequestBody String name) {
-        checklistCategoryService.editName(categoryId, name);
+        categoryService.editName(categoryId, name);
         return new Result<>("Success");
     }
 
     @DeleteMapping("/checklist-categories/{categoryId}")
     public Result<String> delete(@PathVariable Long categoryId) {
-        checklistCategoryService.delete(categoryId);
+        categoryService.delete(categoryId);
         return new Result<>("Success");
     }
 }
---
+++ src/main/java/com/triple/mytrip/web/checklist/form/ChecklistForm.java
@@ -0,0 +1,10 @@
+package com.triple.mytrip.web.checklist.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ChecklistForm {
+
+    String name;
+}
